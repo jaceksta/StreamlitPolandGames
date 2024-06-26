e646e9ed27f11c7b6a73dd83bc684b006ec13a55
jaceksta <ja.staszak@gmail.com>
2024-06-26 20:31:46 +0200

    maybenow?

--- set_piece.py
+++ set_piece.py
@@ -3,10 +3,10 @@ import numpy as np
 from statsbombpy import sb
 
 def get_set_piece_shots(df):
-    df['player_id'] = df['player_id'].astype('int64')
-    shots_for_set_piece = df[(df['type'] == 'Shot') & (df['play_pattern'].str.contains('From')) & (df['shot_body_part'] == 'Head')][['type', 'team', 'player_id','player', 'play_pattern', 'shot_statsbomb_xg', 'shot_body_part']]
+    #df['player_id'] = df['player_id'].astype('int64')
+    shots_for_set_piece = df[(df['type'] == 'Shot') & (df['play_pattern'].str.contains('From')) & (df['shot_body_part'] == 'Head')][['type', 'team','player', 'play_pattern', 'shot_statsbomb_xg', 'shot_body_part']]
 
-    shots_for_set_piece['player_id'] = shots_for_set_piece['player_id'].astype('int64')
+    #shots_for_set_piece['player_id'] = shots_for_set_piece['player_id'].astype('int64')
 
     return shots_for_set_piece
 
@@ -17,10 +17,9 @@ def generate_player_height(num_players=1):
 def get_lineups(match_id, home_team, away_team):
     lineups = pd.concat([sb.lineups(match_id)[home_team], sb.lineups(match_id)[away_team]])
     lineups['player_height'] = generate_player_height(len(lineups))
-    lineups['player_id'] = lineups['player_id'].astype('int64')
-    return lineups[['player_id', 'player_name', 'player_height']]
+    return lineups[['player_name', 'player_height']]
 
 def merge_shots_heights(df, match_id, home_team, away_team):
     shots = get_set_piece_shots(df)
     lineups = get_lineups(match_id, home_team, away_team)
-    return pd.merge(shots, lineups, on='player_id', how='left')
+    return pd.merge(shots, lineups, left_on='player', right_on = 'player_name', how='left')
